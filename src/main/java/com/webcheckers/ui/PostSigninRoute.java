d4373192aa60dc42e2b3bd4aa2425cce2ad4e128
Michael Taylor <met3530@rit.edu>
2021-10-03 17:00:33 -0400

    edited PlayerLobby to have the list as a hashMap, so the addPlayer method only takes a name as input

--- src/main/java/com/webcheckers/ui/PostSigninRoute.java
+++ src/main/java/com/webcheckers/ui/PostSigninRoute.java
@@ -1,5 +1,7 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
 import com.webcheckers.util.Message;
 import spark.Request;
 import spark.Response;
@@ -11,18 +13,24 @@ import java.util.Objects;
 public class PostSigninRoute implements Route {
     private static final String TITLE_ATTR = "title";
     private static final String PLAYER_NAME_ATTR = "playerName";
+    private static final String MESSAGE_ATTR = "message";
+    private static final String MESSAGE_TYPE_ATTR = "messageType";
+
     private static final String TITLE = "Sign-In";
     private static final String VIEW_NAME = "signin.ftl";
 
     private final TemplateEngine templateEngine;
+    private final PlayerLobby playerLobby;
 
     /**
      * Initialize the route with the template engine
      * @param templateEngine engine that will render the view
      */
-    public PostSigninRoute(TemplateEngine templateEngine) {
+    public PostSigninRoute(PlayerLobby playerLobby, TemplateEngine templateEngine) {
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        Objects.requireNonNull(playerLobby, "playerLobby must not be null");
         this.templateEngine = templateEngine;
+        this.playerLobby = playerLobby;
     }
 
     @Override
