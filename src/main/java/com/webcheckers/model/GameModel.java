7996be1b5154b6319189f2ee920a1a49f72f9bb4
Andrew Chacon <aac7845@rit.edu>
2021-10-25 03:22:53 -0400

    progess on check turn

--- src/main/java/com/webcheckers/model/GameModel.java
+++ src/main/java/com/webcheckers/model/GameModel.java
@@ -1,17 +1,22 @@
 package com.webcheckers.model;
 
 import com.webcheckers.appl.GameManager;
+import com.webcheckers.util.Message;
 
 public class GameModel {
     private final Player red_player;
     private final Player white_player;
     private BoardView board;
     private Piece.Color currentColor;
+    private boolean isRedTurn;
+    private boolean isWhiteTurn;
 
     //initiates the class
     public GameModel(Player red_player, Player white_player){
         this.red_player = red_player;
         this.white_player = white_player;
+        this.isRedTurn = true;
+        this.isWhiteTurn = false;
 
         //put both players into this game, this will also not allow other
         //players to start a game with them
@@ -31,6 +36,18 @@ public class GameModel {
     public BoardView getBoard(){return board;}
 
     public void updateBoard(BoardView newBoard) {this.board = newBoard;}
+
+    public Message checkTurn(Player current){
+        if(current.equals(red_player) && isRedTurn){
+            return Message.info("true");
+        }
+        else if(current.equals(white_player) && isWhiteTurn){
+            return Message.info("true");
+        }
+        else{
+            return Message.error("false");
+        }
+    }
 
 
 }
